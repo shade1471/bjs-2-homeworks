fb42b7b888da7320b993f282dcb073095a5efabc
Andrey Gribanov <shade1471@gmail.com>
2022-04-06 15:46:20 +0700

    Task 2, edit

--- 2.functions/main.js
+++ 2.functions/main.js
@@ -5,3 +5,7 @@ console.log(getArrayParams([1, -4, 7]));
 console.log(makeWork([[10,10,20],[20,19,10]],worker));
 console.log(makeWork([[12,23,31],[41,5,61]],worker));
 console.log(makeWork([[14,21,36],[15,26,39]],worker));
+
+console.log(makeWork([[10,10,20],[20,19,10]],worker2));
+console.log(makeWork([[12,23,31],[41,5,61]],worker2));
+console.log(makeWork([[14,21,36],[15,26,39]],worker2));
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,57 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = Infinity, max = -Infinity, sum = 0, avg;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = Number((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let sumArr = func(arrOfArr[i]);
+    if (sumArr > max) {
+      max = sumArr;
+    }
+  }
+
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity, max = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return Math.abs(max - min);
 }
