1e459210a4d281c48ac1dca802a682fc7c529fd4
Andrey Gribanov <shade1471@gmail.com>
2022-05-06 16:55:58 +0700

    first edit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,10 +1,52 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+
+    let idx = cache.findIndex((item) => item.hash === hash); //ищем элемент, хэш кот. равен нашему хэшу
+    if (idx !== -1) { // если элемент найден
+      console.log("Из кэша: " + cache[idx].result);
+      return "Из кэша: " + cache[idx].result;
+    }
+
+    let result = func(...args); // в кэше результата нет - придётся считать
+    cache.push({ hash, result }); // добавляем элемент с правильной структурой
+    if (cache.length > 5) {
+      cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый) 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timeout;
+  let flag;
+
+  return function (...args) {
+    // if (flag === undefined) {
+    //   func.apply(this, args);
+    //  
+    // }
+
+    if (!flag) {
+      func.apply(this, args);
+      flag = true;
+    }
+    clearTimeout(timeout);
+
+   
+
+
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+      flag = false;
+    }, ms)
+
+  }
 }
 
 function debounceDecorator2(func) {
